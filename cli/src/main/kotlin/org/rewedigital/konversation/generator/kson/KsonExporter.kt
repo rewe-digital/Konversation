214edb94b5971c560e4095790e280d47ec62e0e5
rekire <git@reki.re>
2019-01-31 17:15:32 +0100

    Fix formatting options

--- cli/src/main/kotlin/org/rewedigital/konversation/generator/kson/KsonExporter.kt
+++ cli/src/main/kotlin/org/rewedigital/konversation/generator/kson/KsonExporter.kt
@@ -14,11 +14,7 @@ class KsonExporter(private val filter: String) : Exporter {
         intent.prompt.forEachIterator { part ->
             printer("    {\n")
             printer("      \"type\": \"${part.type}\",\n")
-            printer("      \"variants\": [")
-            if(part.variants.isEmpty()) {
-                printer("]")
-            }
-            printer("\n")
+            printer("      \"variants\": [\n")
             printer(part.variants.joinToString(separator = ",\n        ", prefix = "        ", postfix = "\n      ]\n") {
                 "\"${it.replace("\n", "\\n").replace("\r", "\\r").replace("\"", "\\\" ")}\""
             })
@@ -30,29 +26,53 @@ class KsonExporter(private val filter: String) : Exporter {
         }
         printer("  ],\n")
         printer("  \"suggestions\": [")
-        if(intent.suggestions.isEmpty()) {
-            printer("]")
+        if (intent.suggestions.isEmpty()) {
+            printer("],\n")
+        } else {
+            printer("\n")
+            printer(intent.suggestions.joinToString(separator = ",\n    ", prefix = "    ", postfix = "\n  ],\n") {
+                "\"${it.replace("\n", "\\n").replace("\r", "\\r").replace("\"", "\\\" ")}\""
+            })
         }
-        printer("\n")
-        printer(intent.suggestions.joinToString(separator = ",\n    ", prefix = "    ", postfix = "\n  ],\n") {
-            "\"${it.replace("\n", "\\n").replace("\r", "\\r").replace("\"", "\\\" ")}\""
-        })
         printer("  \"reprompts\": {")
-        if(intent.suggestions.isEmpty()) {
-            printer("}")
+        if (intent.reprompt.isEmpty()) {
+            printer("}\n")
+        } else {
+            printer("\n")
+            printer(intent.reprompt.entries.joinToString(separator = ",\n", postfix = "\n  }\n") { entry ->
+                entry.value.first().variants.joinToString(separator = ",\n      ", prefix = "    \"${entry.key}\": [\n      ", postfix = "\n    ]") {
+                    "\"${it.replace("\n", "\\n").replace("\r", "\\r").replace("\"", "\\\" ")}\""
+                }
+            })
         }
-        printer("\n")
-        printer(intent.reprompt.entries.joinToString(separator = ",\n", postfix = "\n  }\n") { entry ->
-            entry.value.first().variants.joinToString(separator = ",\n      ", prefix = "    \"${entry.key}\": [\n      ", postfix = "\n    ]") {
-            "\"${it.replace("\n", "\\n").replace("\r", "\\r").replace("\"", "\\\" ")}\""
-        }
-        })
 
         printer("}")
     }
 
     override fun minified(printer: Printer, intents: MutableList<Intent>) {
-        // TODO
-        prettyPrinted(printer, intents)
+        printer("{\"parts\":[")
+        val intent = intents.first { it.name == filter }
+        intent.prompt.forEachIterator { part ->
+            printer("{\"type\":\"${part.type}\",\"variants\":[")
+            printer(part.variants.joinToString(separator = ",", postfix = "]") {
+                "\"${it.replace("\n", "\\n").replace("\r", "\\r").replace("\"", "\\\" ")}\""
+            })
+            printer("}")
+            if (hasNext()) {
+                printer(",")
+            }
+        }
+        printer("],\"suggestions\":[")
+        printer(intent.suggestions.joinToString(separator = ",", postfix = "],") {
+            "\"${it.replace("\n", "\\n").replace("\r", "\\r").replace("\"", "\\\" ")}\""
+        })
+        printer("\"reprompts\":{")
+        printer(intent.reprompt.entries.joinToString(separator = ",", postfix = "}") { entry ->
+            entry.value.first().variants.joinToString(separator = ",", prefix = "\"${entry.key}\":[", postfix = "]") {
+                "\"${it.replace("\n", "\\n").replace("\r", "\\r").replace("\"", "\\\" ")}\""
+            }
+        })
+
+        printer("}")
     }
 }
--- cli/src/test/kotlin/org/rewedigital/konversation/generator/kson/KsonExporterTest.kt
+++ cli/src/test/kotlin/org/rewedigital/konversation/generator/kson/KsonExporterTest.kt
@@ -16,7 +16,7 @@ class KsonExporterTest {
         assertEqualsIgnoringLineBreaks(expectedResult, sb.toString())
     }
 
-    //@Test
+    @Test
     fun minifiedKvsResult() {
         val help = Parser("cli/src/test/resources/help.kvs").intents
         val sb = StringBuilder()
@@ -25,26 +25,8 @@ class KsonExporterTest {
         assertEquals(expectedMinifiedResult, sb.toString())
     }
 
-    //@Test
-    fun prettyGrammarResult() {
-        val help = Parser("cli/src/test/resources/help.grammar").intents
-        val sb = StringBuilder()
-        val exporter = KsonExporter("Help")
-        exporter.prettyPrinted({ line -> sb.append(line) }, help)
-        assertEquals(expectedResult, sb.toString())
-    }
-
-    //@Test
-    fun minifiedGrammarResult() {
-        val help = Parser("cli/src/test/resources/help.grammar").intents
-        val sb = StringBuilder()
-        val exporter = KsonExporter("Help")
-        exporter.minified({ line -> sb.append(line) }, help)
-        assertEquals(expectedMinifiedResult, sb.toString())
-    }
-
     companion object {
         val expectedResult = File("cli/src/test/resources/help-expected-kson-result.json").readText()
-        val expectedMinifiedResult = File("cli/src/test/resources/help-expected-alexa-result-minified.json").readText()
+        val expectedMinifiedResult = File("cli/src/test/resources/help-expected-kson-result-minified.json").readText()
     }
 }

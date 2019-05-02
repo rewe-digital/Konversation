140fba14cd6a18b1d1ca1311d9c12e1729bad8ae
rekire <git@reki.re>
2019-05-02 23:29:10 +0200

    First draft of the installer for chocolatey

---
+++ cli-integrations/chocolatey/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$binPath = "$toolsPath\..\..\..\bin"
+
+Remove-Item "$toolsPath\konversation.jar" -ea 0
+Remove-Item "$binPath\konversation.bat" -ea 0
+
+Invoke-WebRequest -Uri https://github.com/rewe-digital-incubator/Konversation/releases/download/1.0.1-beta1/konversation-cli.jar -OutFile "$toolsPath\konversation.jar"
+"@java -jar $toolsPath\konversation.jar %1 %2 %3 %4 %5 %6 %7 %8 %9" | out-file -encoding ASCII "$binPath\konversation.bat"
---
+++ cli-integrations/chocolatey/tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$binPath = "$toolsPath\..\..\..\bin"
+
+Remove-Item "$toolsPath\konversation.jar" -ea 0
+Remove-Item "$binPath\konversation.bat" -ea 0

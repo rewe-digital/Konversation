fcec9aa8cf3a031fb80e7c1d0097515c372d406b
rekire <git@reki.re>
2019-05-03 13:09:09 +0200

    Improve installer

--- cli-integrations/chocolatey/tools/chocolateyInstall.ps1
+++ cli-integrations/chocolatey/tools/chocolateyInstall.ps1
@@ -1,10 +1,6 @@
-﻿$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop' # Recommended to use, so the script will stop if there is a problem
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
-$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$binPath = "$toolsPath\..\..\..\bin"
-
-Remove-Item "$toolsPath\konversation.jar" -ea 0
-Remove-Item "$binPath\konversation.bat" -ea 0
-
-Invoke-WebRequest -Uri https://github.com/rewe-digital-incubator/Konversation/releases/download/1.0.1-beta1/konversation-cli.jar -OutFile "$toolsPath\konversation.jar"
-"@java -jar $toolsPath\konversation.jar %1 %2 %3 %4 %5 %6 %7 %8 %9" | out-file -encoding ASCII "$binPath\konversation.bat"
+"@java -jar $toolsDir\konversation.jar %1 %2 %3 %4 %5 %6 %7 %8 %9" | out-file -encoding ASCII "$toolsDir\konversation.bat"
+Install-BinFile -Name "konversation" -Path "$toolsDir\konversation.bat"
+#Install-BinFile -Name "konversation" -Path "$env:JAVA_HOME\bin\java.exe" -useStart -Command "`"-jar $toolsDir\konversation.jar`""
--- cli-integrations/chocolatey/tools/chocolateyUninstall.ps1
+++ cli-integrations/chocolatey/tools/chocolateyUninstall.ps1
@@ -1,7 +1,1 @@
-﻿$ErrorActionPreference = 'Stop'
-
-$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$binPath = "$toolsPath\..\..\..\bin"
-
-Remove-Item "$toolsPath\konversation.jar" -ea 0
-Remove-Item "$binPath\konversation.bat" -ea 0
+﻿Uninstall-BinFile "konversation"

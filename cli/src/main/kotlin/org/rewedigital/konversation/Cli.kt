cae13969b6fd039b6bcc0d68bbb396f4b036f60f
rekire <git@reki.re>
2019-05-17 12:41:48 +0200

    Version update

--- cli/src/main/kotlin/org/rewedigital/konversation/Cli.kt
+++ cli/src/main/kotlin/org/rewedigital/konversation/Cli.kt
@@ -109,7 +109,7 @@ open class Cli {
                         "dump",
                         "-dump" -> dumpOnly = true
                         "-v",
-                        "-version" -> L.log("Konversation CLI version 1.0.1")
+                        "-version" -> L.log("Konversation CLI version 1.1.0-rc1")
                         else -> L.error("Unknown argument \"$arg\".")
                     }
                 }

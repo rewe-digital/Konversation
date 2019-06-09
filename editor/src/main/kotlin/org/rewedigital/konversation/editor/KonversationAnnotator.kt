4f0170396cab7b21371c2aeedfde6896e8457613
rekire <git@reki.re>
2019-06-09 20:22:44 +0200

    Check for simple errors in konversation files

--- editor/src/main/kotlin/org/rewedigital/konversation/editor/KonversationAnnotator.kt
+++ editor/src/main/kotlin/org/rewedigital/konversation/editor/KonversationAnnotator.kt
@@ -5,6 +5,7 @@ import com.intellij.lang.annotation.Annotator
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
+import org.rewedigital.konversation.editor.psi.KonversationUtterence
 
 class KonversationAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
@@ -24,6 +25,18 @@ class KonversationAnnotator : Annotator {
             }
             println("KtCallExpression. Found: $methodName")
         }
+        (element as? KonversationUtterence)?.let { utterance ->
+            println(utterance.text)
+            val slots = validate(utterance.text, element.textRange, holder)
+            var count = 1
+            slots.forEach { slot ->
+                count *= slot.count { it == '|' } + 1
+            }
+            if (count > 1000) {
+                val range = TextRange(element.textRange.startOffset, element.textRange.endOffset)
+                holder.createWarningAnnotation(range, "This utterance creates $count permutations.")
+            }
+        }
     }
 
     private fun validateArgument(argument: PsiElement, holder: AnnotationHolder) {
@@ -39,5 +52,65 @@ class KonversationAnnotator : Annotator {
                 holder.createInfoAnnotation(range, "Hallo!")
             }
         }
+    }
+
+    private fun validate(line: String, textRange: TextRange, holder: AnnotationHolder): MutableList<String> {
+        // Parse the line to make sure that there is no syntax error. Regex would not work for cases like {{Foo}|{Bar}}
+        var start = 0
+        var counter = 0
+        val slots = mutableListOf<String>()
+        var lastWasMasked = false
+        var dontProcess = false
+        line.forEachIndexed { i, c ->
+            when (c) {
+                '\\' -> lastWasMasked = true
+                '{' -> {
+                    if (!lastWasMasked) {
+                        when (counter) {
+                            0 -> start = i + 1
+                            1 -> {
+                                // we found a slot type, that is fine
+                            }
+                            else -> {
+                                val range = TextRange(textRange.startOffset + start, textRange.startOffset + i)
+                                holder.createErrorAnnotation(range, "Too many opening brackets!")
+                            }
+                        }
+                        counter++
+                    }
+                    lastWasMasked = false
+                }
+                '}' -> {
+                    if (!lastWasMasked) {
+                        when (counter) {
+                            1 -> {
+                                // we found the end of the slot
+                                if (!dontProcess) {
+                                    slots.add(line.substring(start, i))
+                                }
+                            }
+                            else -> {
+                                val range = TextRange(textRange.startOffset + i, textRange.startOffset + i + 1)
+                                holder.createErrorAnnotation(range, "Unexpected closing bracket!")
+                            }
+                        }
+                        counter--
+                    }
+                    lastWasMasked = false
+                    dontProcess = false
+                }
+                '$' -> dontProcess = true
+                //'%'-> {
+
+                //}
+                else -> lastWasMasked = false
+            }
+        }
+        if (counter > 0) {
+            val range = TextRange(textRange.startOffset + start - 1, textRange.endOffset)
+            holder.createErrorAnnotation(range, "Closing bracket is missing!")
+        }
+
+        return slots
     }
 }

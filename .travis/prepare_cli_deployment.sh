7552dd99ac0afb37b8c6e12a8b08398a769436a3
rekire <git@reki.re>
2019-09-29 22:21:37 +0200

    Update version numbers

--- .travis/prepare_cli_deployment.sh
+++ .travis/prepare_cli_deployment.sh
@@ -8,8 +8,11 @@ sed -i -E "s/^version '[^']+'$/version '$VERSION'/g" cli/build.gradle
 git add build.gradle
 
 sed -i -E "s/version is [0-9][^]\"]+/version is $VERSION/g" cli/readme.md
-sed -i -E "s/cli-[0-9][^ ]+-blue/`echo $TAG|sed -e "s/-/--/g"`-blue/g" cli/readme.md
+sed -i -E "s/cli-[0-9][^ ]+-blue/`echo ${TAG}|sed -e "s/-/--/g"`-blue/g" cli/readme.md
 git add cli/readme.md
+
+sed -i -e "s/versions.cli = '.*'/versions.cli = '$VERSION'/g" build.gradle
+git add build.gradle
 
 echo Prepairing artifacts...
 # Add link to the not yet created artifact

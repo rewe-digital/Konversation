3f1ff0c25c947750d9a01d20a1ce36b09c42b840
rekire <git@reki.re>
2019-09-30 07:39:59 +0200

    Fix broken chocolaty build

--- .travis/prepare_cli_deployment.sh
+++ .travis/prepare_cli_deployment.sh
@@ -5,7 +5,7 @@ VERSION=`echo ${TAG}|cut -d- -f 2-`
 
 echo Updating version numbers...
 sed -i -E "s/^version '[^']+'$/version '$VERSION'/g" cli/build.gradle
-git add build.gradle
+git add cli/build.gradle
 
 sed -i -E "s/version is [0-9][^]\"]+/version is $VERSION/g" cli/readme.md
 sed -i -E "s/cli-[0-9][^ ]+-blue/`echo ${TAG}|sed -e "s/-/--/g"`-blue/g" cli/readme.md

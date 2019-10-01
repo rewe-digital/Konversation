4beb8f808198272e4b0055da504e31f40e2d9259
rekire <git@reki.re>
2019-10-01 12:10:31 +0200

    Change the execution time of the prepare steps

--- .travis/configure.sh
+++ .travis/configure.sh
@@ -3,10 +3,20 @@ TAG=`git describe --tags`
 MODULE=`echo ${TAG}|cut -d- -f 1`
 VERSION=`echo ${TAG}|cut -d- -f 2-`
 
-echo "Configuring $MODULE for release $VERSION..."
 mkdir -p publish
 echo "bintray.user=$bintry_user">>~/.gradle/gradle.properties
 echo "bintray.key=$bintry_key">>~/.gradle/gradle.properties
 echo "gradle.publish.key=$gradle_key">>~/.gradle/gradle.properties
 echo "gradle.publish.secret=$gradle_secret">>~/.gradle/gradle.properties
 echo "//registry.npmjs.org/:_authToken=$npm_token">~/.npmrc
+
+echo "Configuring $MODULE for release $VERSION..."
+# Hide the mail address from spam bots
+git config --local user.email "`echo "Ym90QHJla2kucmU=" | base64 -d`"
+git config --local user.name "Travis CI"
+git remote set-url origin git@github.com:${TRAVIS_REPO_SLUG}.git
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+
+if [[ -f ".travis/prepare_${MODULE}_deployment.sh" ]]; then
+  .travis/prepare_${MODULE}_deployment.sh
+fi
--- .travis/prepare_deployment.sh
+++ .travis/prepare_deployment.sh
@@ -10,18 +10,8 @@ eval `ssh-agent -s`
 ssh-add travis_rsa
 
 echo Committing changes...
-# Hide the mail address from spam bots
-git config --local user.email "`echo "Ym90QHJla2kucmU=" | base64 -d`"
-git config --local user.name "Travis CI"
-git remote set-url origin git@github.com:${TRAVIS_REPO_SLUG}.git
-git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git fetch
 git checkout master
-
-if [[ -f ".travis/prepare_${MODULE}_deployment.sh" ]]; then
-  .travis/prepare_${MODULE}_deployment.sh
-fi
-
 git commit -m "[skip ci] Publish $MODULE version $VERSION"
 git push
 git tag -fa ${TAG} -m "Release $MODULE $VERSION"
